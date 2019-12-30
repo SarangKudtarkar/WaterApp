bedee66265fc0c55cca0a36b21e67f168c6a7e37
Sarang <sarangkudtarkar49@gmail.com>
2019-12-30 13:57:49 +0530

    Contact feature todo added Lets the user contact shop owner in case of any query

--- app/src/main/java/com/example/acer/waterapp/MainActivity.java
+++ app/src/main/java/com/example/acer/waterapp/MainActivity.java
@@ -10,8 +10,12 @@ Button b1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-   //TODO 1:Add onClicklistener to buttons so they can order cans of water and submit button
+   //TODO 1: Add onClicklistener to buttons so they can order cans of water and submit button
         //TODO 2:The info in fields should be saved so that user can submit while reordering directly
         //TODO 3:Let the user view the status of their order
+        /*
+        @todo contact feature
+        @body Lets the user contact owner in case of any query
+         */
     }
 }
